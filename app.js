abd9787e6a85ae76901402a24bf8884db2731898
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-03-30 12:58:00 +0200

    game-board added

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container')
 const flipButton = document.querySelector('#flip-button')
 
+
+
+// option choosing
+let angle = 0
 function flip () {
     const optionShips = Array.from(optionContainer.children)
-    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)')
+    if (angle === 0) {
+        angle = 90
+    } else {
+        angle = 0
+    }
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
+
+//creating boards
+
+const width = 10
+
+function createBoard() {
+   const gameBoardContainer = document.createElement('div')
+   gameBoardContainer.classList.add('game-board')
+   gameBoardContainer.style.backgroundColor = 'pink'
+
+   gamesBoardContainer.append(gameBoardContainer)
+}
+
+createBoard()
 
 flipButton.addEventListener('click', flip)
