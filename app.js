835ca2dcb2c9f7a88ee6a0f5b7070b3628d884af
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-16 17:54:45 +0200

    block added
    using for loop method

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ function createBoard(color, user) {
     gameBoardContainer.id = user
 
 
+    for (let i = 0; i < width * width; i++) {
+       const block = document.createElement('div')
+       block.classList.add('block')
+       block.id = i
+       gameBoardContainer.append(block)
+    }
+
     gamesBoardContainer.append(gameBoardContainer)
 }
 
