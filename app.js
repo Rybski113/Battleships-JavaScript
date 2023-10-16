356b50d2351aa61b0f797e5ed184f3d017eb9e3f
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-16 18:45:56 +0200

    adding shipPiece (Horizontal)

--- app.js
+++ app.js
@@ -55,3 +55,24 @@ const battleship = new Ship('battleship', 4)
 const carrier = new Ship('carrier', 5)
 
 const ships = [destroyer, submarine, cruiser, battleship, carrier]
+
+function addShipPiece(ship) {
+   const allBoardBlocks = document.querySelectorAll('#computer div')
+   let randomBoolean = Math.random() < 0.5
+   let isHorizontal = true
+   let randomStartIndex = Math.floor(Math.random() * width * width)
+   console.log(randomStartIndex)
+
+   let shipBlocks = []
+
+   for (let i = 0; i < ship.length; i++) {
+    if(isHorizontal) {
+        shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]) 
+    }
+   }
+   console.log(shipBlocks)
+}
+
+
+
+addShipPiece(destroyer)
